eff2819b8a5be5293e6d31db51df4f2f8b27bdb8
Thomas Pietschmann <thomas@pietschi.com>
2021-12-20 20:29:53 +0100

    sassc

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
 set :rbenv_ruby, '2.6.3'
 set :rbenv_path, "~/.rbenv"
+set :user, "rails"
 # set :rbenv_type, :user
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
